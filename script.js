57a8397d1f1dea1e5c489b6d614a3c591a6be8b1
IngoHose <indie.hose@outlook.com>
2024-09-22 20:41:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menuToggle');
+    menuToggle.addEventListener('click', function() {
+        document.body.classList.toggle('nav-open');
+    });
+});
