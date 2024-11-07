4465b97bd4686bd7415372eee59fd0c2f3ee1582
IngoHose <indie.hose@outlook.com>
2024-11-07 19:39:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
-  async function fetchCurrentSong() {
-    try {
-      const response = await fetch('https://api.laut.fm/station/indie-hose/current_song');
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const data = await response.json();
-      
-      const songTitle = data.title;
-      const artist = data.artist.name;
-      
+    async function fetchCurrentSong() {
+      try {
+        const response = await fetch('https://api.laut.fm/station/indie-hose/current_song');
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        
+        const songTitle = data.title;
+        const artist = data.artist.name;
+        
+          document.querySelectorAll('.song-title').forEach(element => {
+          element.textContent = `${artist} - ${songTitle}`;
+        });
+      } catch (error) {
+        console.error('Es gab ein Problem mit der Fetch-Operation:', error);
         document.querySelectorAll('.song-title').forEach(element => {
-        element.textContent = `${artist} - ${songTitle}`;
-      });
-    } catch (error) {
-      console.error('Es gab ein Problem mit der Fetch-Operation:', error);
-      document.querySelectorAll('.song-title').forEach(element => {
-        element.textContent = 'Fehler beim Laden der Songdaten';
+          element.textContent = 'Fehler beim Laden der Songdaten';
+        });
+      }
+    }
+  
+    fetchCurrentSong();
+    setInterval(fetchCurrentSong, 30000);
+  
+    const menuToggle = document.getElementById('menuToggle');
+    if (menuToggle) {
+      menuToggle.addEventListener('click', function() {
+        document.body.classList.toggle('nav-open');
       });
     }
-  }
-
-  fetchCurrentSong();
-  setInterval(fetchCurrentSong, 30000);
-
-  const menuToggle = document.getElementById('menuToggle');
-  if (menuToggle) {
-    menuToggle.addEventListener('click', function() {
-      document.body.classList.toggle('nav-open');
-    });
-  }
-});
-
+  });
+  
+  
